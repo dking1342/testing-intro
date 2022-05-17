a9c016f1e6b0da33052e7a2a3c67c2022e74d26a
David <leanmachine42@yahoo.com>
2022-05-16 19:34:17 -0500

    added coverage

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,13 @@ module.exports = async () => {
       },
     },
     setupFilesAfterEnv: ["./src/jest.setup.ts"],
+    coverageThreshold: {
+      global: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100,
+      },
+    },
   };
 };
